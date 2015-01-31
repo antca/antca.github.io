90a99d4f93a0c5f5c77ebbeb43241a44eec86ef7
Anthony Camboni <anthony@camboni.be>
2015-01-31 15:08:46 +0100

    Modify resume and add pdf format

--- scripts/client/components/Contact.js
+++ scripts/client/components/Contact.js
@@ -8,7 +8,7 @@ var Contact = React.createClass({
     return (
         <div className="row contact_wrap sheet-container">
           {this.getQuery().sent ? <div className="alert alert-success" role="alert"> {this.props.loc["contact_form"]["success_message"]} </div> : null}
-          <div className="col-md-8 col-md-offset-2">
+          <div className="col-md-8">
             <form action="https://getsimpleform.com/messages?form_api_token=8f91eaad9fcad211624674b8c59cfcf3" method="post">
               <input type="hidden" name="redirect_to" value={window.location + "?sent=true"} />
               <div className="form-group" >
@@ -25,6 +25,50 @@ var Contact = React.createClass({
               </div>
               <input className="btn" type="submit" value={this.props.loc["contact_form"]["send"]} />
             </form>
+          </div>
+           <!-- CONTACT -->
+          <div className="col-md-4">
+          <div className="box">
+            <h2><i className="fa fa-bullseye ico"></i> Contact</h2>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-envelope fa-fw"></span></div>
+              <div className="title only pull-right"><a href="#/contact">anthony(at)camboni.be</a></div>
+            </div>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-globe fa-fw"></span>
+              </div>
+              <div className="title only pull-right"><a href="http://anthony.camboni.be" target="_blank">http://anthony.camboni.be</a>
+              </div>
+            </div>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-linkedin-square fa-fw"></span>
+              </div>
+              <div className="title pull-right">LinkedIn</div>
+              <div className="description pull-right"><a href="http://be.linkedin.com/in/antca" target="_blank">Anthony Camboni</a>
+              </div>
+            </div>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-twitter-square fa-fw"></span>
+              </div>
+              <div className="title pull-right">Twitter</div>
+              <div className="description pull-right"><a href="https://twitter.com/anthony_camboni" target="_blank">anthony_camboni</a>
+              </div>
+            </div>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-github-square fa-fw"></span>
+              </div>
+              <div className="title pull-right">GitHub</div>
+              <div className="description pull-right"><a href="https://github.com/antca" target="_blank">antca (new)</a>
+              </div>
+            </div>
+            <div className="contact-item">
+              <div className="icon pull-left text-center"><span className="fa fa-github-square fa-fw"></span>
+              </div>
+              <div className="title pull-right">GitHub</div>
+              <div className="description pull-right"><a href="https://github.com/Apoxx" target="_blank">Apoxx (old)</a>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
     )
--- scripts/client/components/Resume.js
+++ scripts/client/components/Resume.js
@@ -20,6 +20,7 @@ var Resume = React.createClass({
     render() {
       return (
         <div className="resume_wrap" >
+          <div className="resume_pdf"><a className="fa fa-file-pdf-o" href={`public/resume/anthony_camboni_resume_${this.props.lang}.pdf`}></a></div>
           <div className="sheet-container" ref="container" />
           <div className="resume_credit sheet-container">Credit to M. Adam Kendall for the JSON Resume theme (<a href="https://github.com/LinuxBozo/jsonresume-theme-kendall">LinuxBozo@GitHub</a>)</div>
         </div>
